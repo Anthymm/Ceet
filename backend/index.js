2d8a861b0929aa40ff3518d84f6004d0909832af
Anthymm <ells.anton04@gmail.com>
2024-10-31 15:11:05 +0100

    Initialise Docker

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv'),
+  { Client } = require('pg')
+
+dotenv.config()
+
+const client = new Client({
+  connectionString: process.env.PGURI
+})
+
+client.connect()
+
+const express = require('express'),
+  path = require('path')
+
+const app = express(),
+  port = process.env.PORT || 3000
+
+app.use(express.static(path.join(path.resolve(), 'dist')))
+
+app.listen(port, () => {
+  console.log(`Redo på http://localhost:${port}/`)
+})
