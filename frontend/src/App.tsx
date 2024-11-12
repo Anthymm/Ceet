211530608e2fa9569b1d61350259870585659428
Sliepix <victor.ande@icloud.com>
2024-11-12 15:01:15 +0100

    sidebar start

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
-import ProfileComponent from "./components/ProfileComponent";
+import { useState } from "react"
+import ProfileComponent from "./components/ProfileComponent"
+import SidebarComponent from "./components/SidebarComponent"
+import { ImSearch } from "react-icons/im"
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <nav className="flex w-100 justify-end h-[8vh] bg-lightblack items-center shadow">
+        <ImSearch className="text-white" />
         <ProfileComponent name="Admin Admin" />
       </nav>
+      <div className="h-screen w-screen">
+        <SidebarComponent />
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/src/components/SidebarComponent.tsx
+++ frontend/src/components/SidebarComponent.tsx
@@ -1,1 +1,25 @@
-function SidebarComponent() {}
+import { MdKeyboardArrowLeft } from "react-icons/md"
+import { MdKeyboardArrowRight } from "react-icons/md"
+
+function SidebarComponent() {
+  return (
+    <div className=" w-[15%] h-full bg-black  ">
+      {/* <div className="w-full  flex justify-end">
+        <MdKeyboardArrowLeft className="text-white size-10" />
+      </div> */}
+      <div className="flex flex-col items-center">
+        <div className="flex w-52 bg-primary h-16 rounded-lg items-center justify-center cursor-pointer font-medium text-2xl mt-6">
+          Feed
+        </div>
+        <div className="text-white">______________</div>
+        <div>
+          <div className="bg-gray text-white w-36 h-12 rounded-md items-center flex justify-center mt-2 ">
+            Lorem ipsum
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SidebarComponent
