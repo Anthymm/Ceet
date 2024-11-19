a2dd0798ce1cad7c08a92cf340198ef87fe247da
Sliepix <victor.ande@icloud.com>
2024-11-19 13:47:14 +0100

    Merge branch 'Victor-Frontend' of https://github.com/Anthymm/Ceet into Victor-Frontend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 //Postgres
-const dotenv = require('dotenv'),
-  { Client } = require('pg')
+const dotenv = require("dotenv"),
+  { Client } = require("pg")
 
 dotenv.config()
 
 const client = new Client({
-  connectionString: process.env.PGURI
+  connectionString: process.env.PGURI,
 })
 
 client.connect()
@@ -14,26 +14,25 @@ module.exports = { client }
 //
 
 //Express
-const express = require('express'),
-  path = require('path')
+const express = require("express"),
+  path = require("path")
 
 const app = express(),
   port = process.env.PORT || 3000
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(path.resolve(), 'dist')))
+app.use(express.static(path.join(path.resolve(), "dist")))
 //
 
 //Listeners
-const userRoutes = require("./routes/userRoutes");
+const userRoutes = require("./routes/userRoutes")
 const feedRoutes = require("./routes/feedRoutes")
 
-app.use(userRoutes);
+app.use(userRoutes)
 app.use(feedRoutes)
-
 
 //Check ready
 app.listen(port, () => {
