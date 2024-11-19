1c8e7c5ddc549cef980e0b0836e5c236977e7117
Anthymm <ells.anton04@gmail.com>
2024-11-19 13:13:57 +0100

    working compose, dockerfile and nginx

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18-alpine
+FROM node:18-alpine AS backend
 WORKDIR /app
 COPY . /app
 WORKDIR /app/backend
@@ -7,3 +7,8 @@ RUN npm install --prefix ../frontend
 RUN npm run build-frontend
 EXPOSE 3000
 ENTRYPOINT ["node", "index.js"]
+
+FROM nginx:latest AS nginx
+COPY ./nginx.conf /etc/nginx/conf.d/default.conf
+COPY --from=backend /app/backend/dist /etc/nginx//html
+EXPOSE 80
--- frontend/src/Integrity.tsx
+++ frontend/src/Integrity.tsx
@@ -2,6 +2,18 @@ export default function Integrity() {
 
   return (
     <section className="w-full h-full flex justify-start items-center flex-col">
+      <ul>Du som registrerar dig hos CEET godkänner till följande hantering av inlämnade personuppgifter:
+        <li>Din mail kommer att användas för att skicka eventuell konto-borttagning eller lösenords-bytes konfirmationer</li>
+        <li> Din ålder kommer att användas av evenemangs värdar för att kunna stämma av den ungefära åldern på dem som kommer på träffarna
+        </li>
+        <li>När man väl är inloggad på CEET så kan man lägga till Registrerings
+          Nummer till sin/sina bil/bilar för att kunna visa upp dem på sin profil
+          eller för att värdar ska kunna se vilka bilar som kommer.</li>
+      </ul>
+
+
+
+
     </section >
   )
 }
